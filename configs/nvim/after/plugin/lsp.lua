ac23290d40134abe9697b22ce9a20b6b97f2be6a
Braden King <braden@brazenbraden.com>
2023-11-13 04:22:48 +0000

    Update behaviour of Enter key on lsp cmp

--- configs/nvim/after/plugin/lsp.lua
+++ configs/nvim/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ lsp.setup()
 cmp.setup({
   mapping = {
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+    ['<Enter>'] = cmp.mapping.confirm({ select = false }),
 
     ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item()),
     ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item()),
